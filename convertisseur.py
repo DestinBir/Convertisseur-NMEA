62d5f4df2a5f8f3dd4500f5d21ae4a62d9f2d90f
Biringanine Destin <biringaninedestin@gmail.com>
2024-09-30 18:16:12 +0200

    update convertisseur

--- convertisseur.py
+++ convertisseur.py
@@ -32,14 +32,14 @@ def analyser_gpgga(gpgga):
 def generer_gpgsa(gpgga):
     donnees_gpgga = analyser_gpgga(gpgga)
     mode = "A"
-    fix_type = donnees_gpgga['qualite']
+    fix_type = '3'
     prns = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
     prns = prns[:int(donnees_gpgga['satellites'])]
     while len(prns) < 12:
         prns.append('')
-    pdop = round(random.uniform(1, 1.9), 1)
-    hdop = round(random.uniform(1, 1.9), 1)
-    vdop = round(random.uniform(1, 1.9), 1)
+    pdop = '1.0'
+    hdop = '1.0'
+    vdop = '1.0'
     gpgsa = f"$GPGSA,{mode},{fix_type}," + ",".join(prns) + f",{pdop},{hdop},{vdop}*30"
     return gpgsa
 
@@ -47,12 +47,12 @@ def generer_gprmc(gpgga):
     donnees_gpgga = analyser_gpgga(gpgga)
     maintenant = time.strftime("%H%M%S", time.gmtime())
     status = 'A'
-    latitude = f"{donnees_gpgga['latitude']}{donnees_gpgga['direction_latitude']}"
-    longitude = f"{donnees_gpgga['longitude']}{donnees_gpgga['direction_longitude']}"
-    vitesse = f"{random.uniform(0, 100):05.1f}"
-    angle = f"{random.uniform(0, 360):05.1f}"
+    latitude = f"{donnees_gpgga['latitude']},{donnees_gpgga['direction_latitude']}"
+    longitude = f"{donnees_gpgga['longitude']},{donnees_gpgga['direction_longitude']}"
+    vitesse = ""
+    angle = ""
     date = time.strftime("%d%m%y", time.gmtime())
-    variationMagn = f"{random.uniform(0, 15):05.1f},W"
+    variationMagn = "000.0,W"
     gprmc = f"GPRMC,{maintenant},{status},{latitude},{longitude},{vitesse},{angle},{date},{variationMagn}"
     checksum = calcul_checksum(gprmc)
     return f"${gprmc}*{checksum}"
